16f716800a233fb7779719be92d51e394c8e5696
Saif Alqady <saifalqady@192.168.1.8>
2023-11-09 13:32:55 +0200

    deleting footer from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 // Home Page
 "use client";
 import React from "react";
-import { Hero, Footer } from "@/components/modules";
+import { Hero} from "@/components/modules";
 
 const Home = () => {
   return (
     <main className="app-page-container  dark:bg-app-dark dark:text-white border min-h-full">
       <Hero />
-      <Footer />
     </main>
   );
 };
