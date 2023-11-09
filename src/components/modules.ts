194a680270f5291be32714da3085f9dc1b94de1d
Saif Alqady <saifalqady@192.168.1.8>
2023-11-09 13:17:27 +0200

    fixing theme provider issue

--- src/components/modules.ts
+++ src/components/modules.ts
@@ -4,6 +4,7 @@ export { default as Header } from "./common/header";
 export { default as Hero } from "./hero";
 export { default as Footer } from "./common/footer";
 export { default as CompanyCard } from "./company/companyCard";
+export {default as ToggleTheme } from "./common/toggleTheme";
 
 // Common components
 
